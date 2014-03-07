da470cc66c81f0c887d31c1f9c3dbf00551fbad6
Anh <Anh@dhcp-39-2-38.dhcpint.csulb.edu>
2014-03-07 11:34:05 -0800

    Conflict

--- MonopolyEmpire.cpp
+++ MonopolyEmpire.cpp
@@ -10,6 +10,7 @@ int main()
     // ADDING ONE MORE
     // Added Anthony
     // testing
+    // Addeding conflict
 {
 	cout << "Welcome to Monopoly Empire!" << endl << "Enter the number of players(1-4): ";
 
